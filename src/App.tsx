49facee73f48e2d8ce51f03dfebd57ef70b35497
Nick <61873872+LedinN@users.noreply.github.com>
2024-03-18 08:58:27 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { useCounterStore } from './store';
+
+function App() {
+  const count = useCounterStore((state) => state.count);  
+
+  return <OtherComponent count={count} />;
+}
+
+const OtherComponent = ({ count }: { count: number }) => {
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return <div>
+    {count}
+    <div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+    </div>;
+}
+
+export default App
